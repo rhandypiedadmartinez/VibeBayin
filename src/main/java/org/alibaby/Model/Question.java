a1835e89ac14741e3324296c14b8473a663978e9
AiriGotou <85325626+AiriGotou@users.noreply.github.com>
2023-07-02 11:36:15 +0800

    Update Question.java

--- src/main/java/org/alibaby/Model/Question.java
+++ src/main/java/org/alibaby/Model/Question.java
@@ -19,13 +19,25 @@ public class Question {
     @PropertyName("answer")
     public String answer;
 
-    public Question(){}
-    
-    public Question(String question, String choice1, String choice2, String choice3, String answer){
+    public Question() {
+    }
+
+    public Question(String question, String choice1, String choice2, String choice3, String answer) {
         this.question = question;
         this.choice1 = choice1;
         this.choice2 = choice2;
         this.choice3 = choice3;
         this.answer = answer;
+    }
+
+    public static Question fromCSV(String csvLine) {
+        String[] values = csvLine.split(",");
+        String question = values[0];
+        String choice1 = values[1];
+        String choice2 = values[2];
+        String choice3 = values[3];
+        String answer = values[4];
+
+        return new Question(question, choice1, choice2, choice3, answer);
     }
 }
