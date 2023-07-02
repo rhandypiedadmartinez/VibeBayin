dfc75dbe4788a7f557a817d69d22f70411c68460
AiriGotou <85325626+AiriGotou@users.noreply.github.com>
2023-07-02 11:36:47 +0800

    Update QuestionMethod.java

--- src/main/java/org/alibaby/Playground/QuestionMethod.java
+++ src/main/java/org/alibaby/Playground/QuestionMethod.java
@@ -1,15 +1,12 @@
 package org.alibaby.Playground;
 
 import org.alibaby.Model.Question;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class QuestionMethod extends Question {
-    public QuestionMethod(String question, String choice1, String choice2, String choice3, String answer) {
-        super(question, choice1, choice2, choice3, answer);
-    }
-
     public static void main(String[] args) {
 
         String csvFile = "resources\\javafx-sdk-20.0.1\\lib\\quiz.csv";
@@ -17,18 +14,18 @@ public class QuestionMethod extends Question {
         String csvSplitBy = ",";
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-        while ((line = br.readLine()) != null) {
-         String[] values = line.split(csvSplitBy);
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(csvSplitBy);
 
-        // Process the values as needed
-        // For example, print them
-         for (String value : values) {
-           System.out.print(value + " ");
+                // Process the values as needed
+                // For example, print them
+                for (String value : values) {
+                    System.out.print(value + " ");
+                }
+                System.out.println();
             }
-          System.out.println();
-              }
-          } catch (IOException e) {
-               e.printStackTrace();
-          }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
